991e66bfeb0e25a2e7f94bff4b7418b3991f52ae
eakudompong.chanoknan <ceguantz96@outlook.com>
2025-01-30 15:01:37 +0900

    add pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import react from "@astrojs/react";
 
 import node from "@astrojs/node";
 
+import AstroPWA from "@vite-pwa/astro";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -15,6 +17,36 @@ export default defineConfig({
     }),
     ,
     react(),
+    AstroPWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "ECCGakuApp改",
+        short_name: "GakuApp改",
+        description: "ECCコンピュータ専門学校学生アプリ",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
   ],
 
   output: "server",
