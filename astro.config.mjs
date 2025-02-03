aada3019e8aa01a870cca267aa10dce3d4a6feea
eakudompong.chanoknan <ceguantz96@outlook.com>
2025-02-03 14:44:10 +0900

    remove pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,6 @@ import react from "@astrojs/react";
 
 import node from "@astrojs/node";
 
-import AstroPWA from "@vite-pwa/astro";
-
 // https://astro.build/config
 export default defineConfig({
   server: {
@@ -21,37 +19,6 @@ export default defineConfig({
     }),
     ,
     react(),
-    AstroPWA({
-      registerType: "autoUpdate",
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: ({ url }) => url.pathname.startsWith("/"),
-            handler: "StaleWhileRevalidate",
-            options: {
-              cacheName: "astro-pwa",
-            },
-          },
-        ],
-      },
-      manifest: {
-        name: "ECCGakuApp改",
-        short_name: "GakuApp改",
-        description: "ECCコンピュータ専門学校学生アプリ",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "/android-chrome.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-        ],
-        display: "standalone",
-      },
-      devOptions: {
-        enabled: true,
-      },
-    }),
   ],
 
   output: "server",
